ec6f3ff2d9fbd98e11b5bf20f522c5e2cb5ce430
sahbajali <sahbaj.ali576@gmail.com>
2020-05-27 12:52:59 +0530

    Cookies

--- app.js
+++ app.js
@@ -25,30 +25,46 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('1234567890-0987654321'));//keep any secret key doesnt matter
 
 function auth(req,res,next) {
-  console.log(req.headers);
-  var authHeader=req.headers.authorization;//authorization is a property in header
-  if(!authHeader){
-    var err=new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status=401;
-    next(err);
-    return;
-  }
-  var auth=new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
-  var user=auth[0];
-  var pass=auth[1];
-  if(user==='admin'&&pass==='password'){
-    next();
+  console.log(req.signedCookies);
+
+  if(!req.signedCookies.user){
+    var authHeader=req.headers.authorization;//authorization is a property in header
+    if(!authHeader){
+      var err=new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate','Basic');
+      err.status=401;
+      next(err);
+      return;
+    }
+    var auth=new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
+    var user=auth[0];
+    var pass=auth[1];
+    if(user==='admin'&&pass==='password'){
+      res.cookie('user','admin',{signed:true})
+      next();
+    }
+    else{
+      var err=new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate','Basic');
+      err.status=401;
+      next(err);
+    }
   }
   else{
-    var err=new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status=401;
-    next(err);
+    if(req.signedCookies.user==='admin'){
+      next();
+    }
+    else{
+      var err=new Error('You are not authenticated!');
+      //res.setHeader('WWW-Authenticate','Basic');
+      err.status=401;
+      next(err);
+    }
   }
+  
 }
 app.use(auth);//adding authentication to the app.auth is the name of the function
 app.use(express.static(path.join(__dirname, 'public')));
